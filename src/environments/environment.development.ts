04a073c32aee953543d0b98744435bb5475a0092
Romulo Pinheiro <romuloe.p.jesus@gmail.com>
2023-12-03 02:56:48 -0300

    @infoDoFut - chore: configure environment variables with ngx-env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: import.meta.env.NG_APP_API_URL_TEST,
+  clientId: import.meta.env.NG_APP_CLIENT_ID,
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: import.meta.env.NG_APP_API_URL,
+  clientId: import.meta.env.NG_APP_CLIENT_ID,
+};
